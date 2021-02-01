1e74389359f9cf8075a6cf3e8932dee299d48f62
Kareem-Khaled <kareemkhaled143@gmail.com>
2021-02-01 18:11:46 +0200

    add

---
+++ SPOJ/WEIRDFN - Weird Function.cpp
@@ -0,0 +1,56 @@
+/*
+¦¦+  ¦¦+ ¦¦¦¦¦+ ¦¦¦¦¦¦+ ¦¦¦¦¦¦¦+¦¦¦¦¦¦¦+¦¦¦+   ¦¦¦+        ¦¦+  ¦¦+¦¦+  ¦¦+ ¦¦¦¦¦+ ¦¦+     ¦¦¦¦¦¦¦+¦¦¦¦¦¦+
+¦¦¦ ¦¦++¦¦+--¦¦+¦¦+--¦¦+¦¦+----+¦¦+----+¦¦¦¦+ ¦¦¦¦¦        ¦¦¦ ¦¦++¦¦¦  ¦¦¦¦¦+--¦¦+¦¦¦     ¦¦+----+¦¦+--¦¦+
+¦¦¦¦¦++ ¦¦¦¦¦¦¦¦¦¦¦¦¦¦++¦¦¦¦¦+  ¦¦¦¦¦+  ¦¦+¦¦¦¦+¦¦¦        ¦¦¦¦¦++ ¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦     ¦¦¦¦¦+  ¦¦¦  ¦¦¦
+¦¦+-¦¦+ ¦¦+--¦¦¦¦¦+--¦¦+¦¦+--+  ¦¦+--+  ¦¦¦+¦¦++¦¦¦        ¦¦+-¦¦+ ¦¦+--¦¦¦¦¦+--¦¦¦¦¦¦     ¦¦+--+  ¦¦¦  ¦¦¦
+¦¦¦  ¦¦+¦¦¦  ¦¦¦¦¦¦  ¦¦¦¦¦¦¦¦¦¦+¦¦¦¦¦¦¦+¦¦¦ +-+ ¦¦¦¦¦¦¦¦¦¦+¦¦¦  ¦¦+¦¦¦  ¦¦¦¦¦¦  ¦¦¦¦¦¦¦¦¦¦+¦¦¦¦¦¦¦+¦¦¦¦¦¦++
++-+  +-++-+  +-++-+  +-++------++------++-+     +-++------++-+  +-++-+  +-++-+  +-++------++------++-----+
+Believe in yourself ,( try and try and then try ), You can do it
+*/
+#define _CRT_SECURE_NO_WARNINGS
+#include<bits/stdc++.h>
+#include<unordered_map>
+#define endl "\n"
+#define Ceil(x,y) ((x+y-1)/y)
+#define clr(arr, x) memset(arr, x, sizeof arr)
+#define all(v) v.begin(),v.end()
+#define allr(v) v.rbegin(),v.rend()
+#define rt(s) return cout<<s,0
+#define sz(s)	(int)(s.size())
+#define OO 0x3f3f3f3f3f3f3f3fLL
+const int oo = 1e5, mod = 1e9 + 7;
+double EBS = 1e-6;
+using namespace std;
+typedef long long ll;
+typedef unsigned long long ull;
+int dx[] { 1, -1, 0, 0, 1, 1, -1, -1 };
+int dy[] { 0, 0, 1, -1, 1, -1, 1, -1 };
+void K_K(){
+#ifndef ONLINE_JUDGE
+	freopen("in.txt", "r", stdin);
+	freopen("out.txt", "w", stdout);
+#endif  !ONLINEJUDGE
+	ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+}
+int main(){
+	K_K();
+	int t; cin >> t;
+	while (t--){
+		ll a, b, c, n; cin >> a >> b >> c >> n;
+		priority_queue<ll> l, r;
+		l.push(1); ll sum = 1;
+		for (int i = 2; i <= n; i++) {
+			ll cur = ((a * l.top()) % mod + (b * i) % mod + c) % mod;
+			sum += cur;
+			if (i % 2)
+				l.push(cur);
+			else
+				r.push(-cur);
+			if (sz(l) && sz(r) && l.top() > -r.top()) {
+				l.push(-r.top()); r.pop();
+				r.push(-l.top()); l.pop();
+			}
+		}
+		cout << sum << endl;
+	}
+}
