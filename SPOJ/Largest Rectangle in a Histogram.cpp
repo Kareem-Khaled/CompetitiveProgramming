1d0e3e7a6fda791bec8eb2bb54cf2812d7fa4b7d
Kareem-Khaled <kareemkhaled143@gmail.com>
2021-02-01 19:03:30 +0200

    add

---
+++ SPOJ/Largest Rectangle in a Histogram.cpp
@@ -0,0 +1,45 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<bits/stdc++.h>
+#include<unordered_map>
+#define endl "\n"
+#define Ceil(x,y) ((x+y-1)/y)
+#define clr(arr, x) memset(arr, x, sizeof arr)
+#define all(v) v.begin(),v.end()
+#define rt(s) return cout<<s,0
+#define sz(s)	(int)(s.size())
+#define OO 0x3f3f3f3f3f3f3f3fLL
+const int oo = 1e5, mod = 1e9 + 7;
+using namespace std;
+typedef long long ll;
+int dx[] { 1, -1, 0, 0, 1, 1, -1, -1 };
+int dy[] { 0, 0, 1, -1, 1, -1, 1, -1 };
+void K_K(){
+#ifndef ONLINE_JUDGE
+	freopen("in.txt", "r", stdin);
+	freopen("out.txt", "w", stdout);
+#endif  !ONLINEJUDGE
+	ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+}
+ 
+int main(){
+	K_K();
+		int n;
+		while (cin >> n && n){
+		vector<int>arr(n);
+		for (auto &it : arr)cin >> it;
+		stack<ll>st, h; ll ans = 0;
+		for (int i = 0; i < n; i++){
+			int lst = oo;
+			if (!sz(st) || arr[i] > h.top())
+				st.push(i), h.push(arr[i]);
+			else{
+				while (sz(st) && arr[i] < h.top())
+					lst = st.top(), ans = max(ans, (i - st.top())*h.top()), st.pop(), h.pop();
+				st.push(min(lst, i)); h.push(arr[i]);
+			}
+		}
+		while (sz(st))
+			ans = max(ans, (n - st.top())*h.top()), st.pop(), h.pop();
+		cout << ans << endl;
+	}
+} 
