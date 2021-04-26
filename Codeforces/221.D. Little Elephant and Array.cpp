7878a403332e570622b984c715b9e222bda09646
Kareem-Khaled <kareemkhaled143@gmail.com>
2021-04-26 20:56:17 +0200

    add

---
+++ Codeforces/221.D. Little Elephant and Array.cpp
@@ -0,0 +1,60 @@
+﻿/*
+██╗  ██╗ █████╗ ██████╗ ███████╗███████╗███╗   ███╗        ██╗  ██╗██╗  ██╗ █████╗ ██╗     ███████╗██████╗
+██║ ██╔╝██╔══██╗██╔══██╗██╔════╝██╔════╝████╗ ████║        ██║ ██╔╝██║  ██║██╔══██╗██║     ██╔════╝██╔══██╗
+█████╔╝ ███████║██████╔╝█████╗  █████╗  ██╔████╔██║        █████╔╝ ███████║███████║██║     █████╗  ██║  ██║
+██╔═██╗ ██╔══██║██╔══██╗██╔══╝  ██╔══╝  ██║╚██╔╝██║        ██╔═██╗ ██╔══██║██╔══██║██║     ██╔══╝  ██║  ██║
+██║  ██╗██║  ██║██║  ██║███████╗███████╗██║ ╚═╝ ██║███████╗██║  ██╗██║  ██║██║  ██║███████╗███████╗██████╔╝
+╚═╝  ╚═╝╚═╝  ╚═╝╚═╝  ╚═╝╚══════╝╚══════╝╚═╝     ╚═╝╚══════╝╚═╝  ╚═╝╚═╝  ╚═╝╚═╝  ╚═╝╚══════╝╚══════╝╚═════╝
+Believe in yourself ,( try and try and then try ), You can do it
+*/
+// Problem Link : https://codeforces.com/contest/221/problem/D
+#define _CRT_SECURE_NO_WARNINGS
+#include<bits/stdc++.h>
+#include<unordered_map>
+#define endl "\n"
+#define Ceil(x,y) ((x+y-1)/y)
+#define clr(arr, x) memset(arr, x, sizeof arr)
+#define all(v) v.begin(),v.end()
+#define allr(v) v.rbegin(),v.rend()
+#define rt(s) return cout<<s,0
+#define sz(s)	(int)(s.size())
+#define oo 0x7FFFFFFF
+#define OO 0x7FFFFFFFFFFFFFFFLL
+using namespace std;
+typedef long long ll;
+int dx[] { 1, -1, 0, 0, 1, 1, -1, -1 };
+int dy[] { 0, 0, 1, -1, 1, -1, 1, -1 };
+void K_K(){
+#ifndef ONLINE_JUDGE
+	freopen("in.txt", "r", stdin);
+	freopen("out.txt", "w", stdout);
+#endif  !ONLINEJUDGE
+	ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+}
+int main(){
+	K_K();
+	int n, m; cin >> n >> m;
+	vector<int>arr(n), nums;
+	map<int, int>frq;
+	for (auto &it : arr){
+		cin >> it;
+		if (it <= n)
+			frq[it]++;
+	}
+	for (auto it : frq){
+		if (it.second >= it.first)
+			nums.push_back(it.first);
+	}
+	vector<vector<int>>prefx(sz(nums), vector<int>(n + 1));
+	for (int i = 0; i < sz(nums); i++){
+		for (int j = 1; j <= n; j++){
+			prefx[i][j] = prefx[i][j - 1] + (arr[j - 1] == nums[i]);
+		}
+	}
+	while (m--){
+		int l, r; cin >> l >> r; int ans = 0;
+		for (int i = 0; i < sz(nums); i++)
+			ans += (prefx[i][r] - prefx[i][l - 1] == nums[i]);
+		cout << ans << endl;
+	}
+}
---
+++ Codeforces/731.C. Socks.cpp
@@ -0,0 +1,70 @@
+﻿/*
+██╗  ██╗ █████╗ ██████╗ ███████╗███████╗███╗   ███╗        ██╗  ██╗██╗  ██╗ █████╗ ██╗     ███████╗██████╗
+██║ ██╔╝██╔══██╗██╔══██╗██╔════╝██╔════╝████╗ ████║        ██║ ██╔╝██║  ██║██╔══██╗██║     ██╔════╝██╔══██╗
+█████╔╝ ███████║██████╔╝█████╗  █████╗  ██╔████╔██║        █████╔╝ ███████║███████║██║     █████╗  ██║  ██║
+██╔═██╗ ██╔══██║██╔══██╗██╔══╝  ██╔══╝  ██║╚██╔╝██║        ██╔═██╗ ██╔══██║██╔══██║██║     ██╔══╝  ██║  ██║
+██║  ██╗██║  ██║██║  ██║███████╗███████╗██║ ╚═╝ ██║███████╗██║  ██╗██║  ██║██║  ██║███████╗███████╗██████╔╝
+╚═╝  ╚═╝╚═╝  ╚═╝╚═╝  ╚═╝╚══════╝╚══════╝╚═╝     ╚═╝╚══════╝╚═╝  ╚═╝╚═╝  ╚═╝╚═╝  ╚═╝╚══════╝╚══════╝╚═════╝
+Believe in yourself ,( try and try and then try ), You can do it
+*/
+// Problem Link: https://codeforces.com/contest/731/problem/C
+#define _CRT_SECURE_NO_WARNINGS
+#include<bits/stdc++.h>
+#include<unordered_map>
+#define endl "\n"
+#define Ceil(x,y) ((x+y-1)/y)
+#define clr(arr, x) memset(arr, x, sizeof arr)
+#define all(v) v.begin(),v.end()
+#define allr(v) v.rbegin(),v.rend()
+#define rt(s) return cout<<s,0
+#define sz(s)	(int)(s.size())
+#define oo 0x7FFFFFFF
+#define OO 0x7FFFFFFFFFFFFFFFLL
+using namespace std;
+typedef long long ll;
+int dx[] { 1, -1, 0, 0, 1, 1, -1, -1 };
+int dy[] { 0, 0, 1, -1, 1, -1, 1, -1 };
+void K_K(){
+#ifndef ONLINE_JUDGE
+	freopen("in.txt", "r", stdin);
+	freopen("out.txt", "w", stdout);
+#endif  !ONLINEJUDGE
+	ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+}
+map<int, int>frq;
+vector<int>color;
+vector<vector<int>>adj;
+int const N = 2e5 + 3;
+bool vis[N]; int mx;
+int dfs(int node){
+	frq[color[node]]++; vis[node] = 1;
+	mx = max(mx, frq[color[node]]);
+	int cnt = 1;
+	for (auto child : adj[node]){
+		if (!vis[child])
+			cnt += dfs(child);
+	}
+	return cnt;
+}
+int main(){
+	K_K();
+	int n, m, k; cin >> n >> m >> k;
+	color.resize(n + 1);
+	for (int i = 1; i <= n; i++){
+		cin >> color[i];
+	}
+	adj.resize(n + 1);
+	while (m--){
+		int l, r; cin >> l >> r;
+		adj[l].push_back(r);
+		adj[r].push_back(l);
+	}
+	int ans = 0;
+	for (int i = 1; i <= n; i++){
+		if (!vis[i]){
+			frq.clear(); mx = 0;
+			ans += dfs(i) - mx;
+		}
+	}
+	cout << ans;
+}
