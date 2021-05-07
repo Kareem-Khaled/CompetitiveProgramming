4e24d7479e30e13e86b4c5dba9ce21e9904624ef
Kareem-Khaled <kareemkhaled143@gmail.com>
2021-05-07 16:46:07 +0200

    add

---
+++ Codeforces/1520 E. Arranging The Sheep.cpp
@@ -0,0 +1,66 @@
+﻿/*
+██╗  ██╗ █████╗ ██████╗ ███████╗███████╗███╗   ███╗        ██╗  ██╗██╗  ██╗ █████╗ ██╗     ███████╗██████╗
+██║ ██╔╝██╔══██╗██╔══██╗██╔════╝██╔════╝████╗ ████║        ██║ ██╔╝██║  ██║██╔══██╗██║     ██╔════╝██╔══██╗
+█████╔╝ ███████║██████╔╝█████╗  █████╗  ██╔████╔██║        █████╔╝ ███████║███████║██║     █████╗  ██║  ██║
+██╔═██╗ ██╔══██║██╔══██╗██╔══╝  ██╔══╝  ██║╚██╔╝██║        ██╔═██╗ ██╔══██║██╔══██║██║     ██╔══╝  ██║  ██║
+██║  ██╗██║  ██║██║  ██║███████╗███████╗██║ ╚═╝ ██║███████╗██║  ██╗██║  ██║██║  ██║███████╗███████╗██████╔╝
+╚═╝  ╚═╝╚═╝  ╚═╝╚═╝  ╚═╝╚══════╝╚══════╝╚═╝     ╚═╝╚══════╝╚═╝  ╚═╝╚═╝  ╚═╝╚═╝  ╚═╝╚══════╝╚══════╝╚═════╝
+Believe in yourself ,( try and try and then try ), You can do it
+*/
+// Problem Link : https://codeforces.com/contest/1520/problem/E
+#define _CRT_SECURE_NO_WARNINGS
+#include<bits/stdc++.h>
+#include<unordered_map>
+#define endl "\n"
+#define Ceil(x,y) ((x+y-1)/y)
+#define clr(arr, x) memset(arr, x, sizeof arr)
+#define all(v) v.begin(),v.end()
+#define allr(s) s.rbegin(),s.rend()
+#define rt(s) return cout<<s,0
+#define watch(x) cout<<(#x)<<" = "<<x<<endl
+#define sz(s)	(int)(s.size())
+#define OO 0x3f3f3f3f3f3f3f3fLL
+#define oo 0x3f3f3f3f
+using namespace std;
+typedef long long ll;
+typedef long double ld;
+typedef unsigned long long ull;
+int dx[] { 1, -1, 0, 0, 1, 1, -1, -1 };
+int dy[] { 0, 0, 1, -1, 1, -1, 1, -1 };
+void K_K(){
+#ifndef ONLINE_JUDGE
+	freopen("in.txt", "r", stdin);
+	freopen("out.txt", "w", stdout);
+#endif  !ONLINEJUDGE
+	ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+}
+int main(){
+	K_K();
+	int t; cin >> t;
+	while (t--){
+		int n; string s; cin >> n >> s;
+		vector<ll>pre(n + 1), suf(n + 2);
+		int cnt = 0; s = '#' + s;
+		for (int i = 1; i <= n; i++){
+			pre[i] += pre[i - 1];
+			if (s[i] != '*')
+				pre[i] += cnt;
+			else
+				cnt++;
+		}
+		cnt = 0;
+		for (int i = n; i > 0; i--){
+			suf[i] += suf[i + 1];
+			if (s[i] != '*')
+				suf[i] += cnt;
+			else
+				cnt++;
+		}
+		ll ans = OO;
+		for (int i = 1; i <= n; i++){
+			ans = min(ans, pre[i] + suf[i + 1]);
+		}
+		cout << ans << endl;
+	}
+
+}
