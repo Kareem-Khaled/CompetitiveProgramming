1da99f3ae9ddaf2af8e9358fb26e102cc87fe144
Kareem <53629881+Kareem-Khaled@users.noreply.github.com>
2021-06-20 18:29:09 +0200

    Update 15. 3Sum.cpp

--- LeetCode/15. 3Sum.cpp
+++ LeetCode/15. 3Sum.cpp
@@ -8,28 +8,6 @@
 Believe in yourself ,( try and try and then try ), You can do it
 */
 // Problem Link : https://leetcode.com/problems/3sum/
-#define _CRT_SECURE_NO_WARNINGS
-#include<bits/stdc++.h>
-#include<unordered_map>
-#define endl "\n"
-#define Ceil(x,y) ((x+y-1)/y)
-#define clr(arr, x) memset(arr, x, sizeof arr)
-#define all(v) v.begin(),v.end()
-#define rt(s) return cout<<s,0
-#define sz(s)	(int)(s.size())
-#define oo 0x7FFFFFFF
-#define OO 0x7FFFFFFFFFFFFFFFLL
-using namespace std;
-typedef long long ll;
-int dx[] { 1, -1, 0, 0, 1, 1, -1, -1 };
-int dy[] { 0, 0, 1, -1, 1, -1, 1, -1 };
-void K_K(){
-#ifndef ONLINE_JUDGE
-	freopen("in.txt", "r", stdin);
-	freopen("out.txt", "w", stdout);
-#endif  !ONLINEJUDGE
-	ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
-}
 class Solution {
 public:
 	vector<vector<int>> threeSum(vector<int>& nums) {
