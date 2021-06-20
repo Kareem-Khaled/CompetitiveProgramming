327fe362f93c8f51f1fbe79b4467e1cae2ae7240
Kareem <53629881+Kareem-Khaled@users.noreply.github.com>
2021-06-20 18:33:36 +0200

    Update 1081.Smallest Subsequence of Distinct Characters.cpp

--- LeetCode/1081.Smallest Subsequence of Distinct Characters.cpp
+++ LeetCode/1081.Smallest Subsequence of Distinct Characters.cpp
@@ -5,52 +5,27 @@
 ██╔═██╗ ██╔══██║██╔══██╗██╔══╝  ██╔══╝  ██║╚██╔╝██║        ██╔═██╗ ██╔══██║██╔══██║██║     ██╔══╝  ██║  ██║
 ██║  ██╗██║  ██║██║  ██║███████╗███████╗██║ ╚═╝ ██║███████╗██║  ██╗██║  ██║██║  ██║███████╗███████╗██████╔╝
 ╚═╝  ╚═╝╚═╝  ╚═╝╚═╝  ╚═╝╚══════╝╚══════╝╚═╝     ╚═╝╚══════╝╚═╝  ╚═╝╚═╝  ╚═╝╚═╝  ╚═╝╚══════╝╚══════╝╚═════╝
-Believe in yourself ,( try and try and then try ), You can do it 💗
+Believe in yourself ,( try and try and then try ), You can do it
 */
 // Problem Link : https://leetcode.com/problems/smallest-subsequence-of-distinct-characters/
-#define _CRT_SECURE_NO_WARNINGS
-#include<bits/stdc++.h>
-#include<unordered_map>
-#define endl "\n"
-#define Ceil(x,y) ((x+y-1)/y)
-#define clr(arr, x) memset(arr, x, sizeof arr)
-#define all(v) v.begin(),v.end()
-#define allr(s) s.rbegin(),s.rend()
-#define rt(s) return cout<<s,0
-#define watch(x) cout<<(#x)<<" = "<<x<<endl
-#define sz(s)	(int)(s.size())
-#define OO 0x3f3f3f3f3f3f3f3fLL
-const int oo = 0x3f3f3f3f, mod = 1e9 + 7;
-using namespace std;
-typedef long long ll;
-typedef long double ld;
-typedef unsigned long long ull;
-int dx[] { 1, -1, 0, 0, 1, 1, -1, -1 };
-int dy[] { 0, 0, 1, -1, 1, -1, 1, -1 };
-void K_K(){
-#ifndef ONLINE_JUDGE
-	freopen("in.txt", "r", stdin);
-	freopen("out.txt", "w", stdout);
-#endif  !ONLINEJUDGE
-	ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
-}
-int main(){
-	K_K();
-	string s; cin >> s;
-	map<char, int>frq, vis;
-	for (int i = 0; i < sz(s); i++){
+class Solution {
+public:
+    string smallestSubsequence(string s) {
+	unordered_map<char, int>frq, vis;
+	for (int i = 0; i < s.size(); i++){
 		frq[s[i]]++;
 	}
 	string ans = ""; stack<char>st;
-	for (int i = 0; i < sz(s); i++){
+	for (int i = 0; i < s.size(); i++){
 		frq[s[i]]--;
 		if (vis[s[i]])continue;
-		while (sz(st) && s[i] < st.top() && frq[st.top()])
+		while (st.size() && s[i] < st.top() && frq[st.top()])
 			vis[st.top()]--, st.pop();
 		st.push(s[i]); vis[s[i]]++;
 	}
-	while (sz(st))
+	while (st.size())
 		ans += st.top(), st.pop();
-	reverse(all(ans));
-	cout << ans;
-}
+	reverse(ans.begin(),ans.end());
+	return ans;
+    }
+};
