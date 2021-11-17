2f00bfc48a52491edbf900bbe7bee01bd04a8441
Kareem <53629881+Kareem-Khaled@users.noreply.github.com>
2021-11-18 01:36:23 +0200

    Update 11404 - Palindromic Subsequence.cpp

--- UVA/11404 - Palindromic Subsequence.cpp
+++ UVA/11404 - Palindromic Subsequence.cpp
@@ -1,4 +1,4 @@
-﻿/*
+/*
 ██╗  ██╗ █████╗ ██████╗ ███████╗███████╗███╗   ███╗        ██╗  ██╗██╗  ██╗ █████╗ ██╗     ███████╗██████╗
 ██║ ██╔╝██╔══██╗██╔══██╗██╔════╝██╔════╝████╗ ████║        ██║ ██╔╝██║  ██║██╔══██╗██║     ██╔════╝██╔══██╗
 █████╔╝ ███████║██████╔╝█████╗  █████╗  ██╔████╔██║        █████╔╝ ███████║███████║██║     █████╗  ██║  ██║
@@ -7,6 +7,7 @@
 ╚═╝  ╚═╝╚═╝  ╚═╝╚═╝  ╚═╝╚══════╝╚══════╝╚═╝     ╚═╝╚══════╝╚═╝  ╚═╝╚═╝  ╚═╝╚═╝  ╚═╝╚══════╝╚══════╝╚═════╝
 Believe in yourself ,( try and try and then try ), You can do it
 */
+// problem Link: https://onlinejudge.org/index.php?option=com_onlinejudge&Itemid=8&page=show_problem&problem=2399
 #define _CRT_SECURE_NO_WARNINGS
 #include<bits/stdc++.h>
 #include<unordered_map>
@@ -19,9 +20,11 @@ Believe in yourself ,( try and try and then try ), You can do it
 #define watch(x) cout<<(#x)<<" = "<<x<<endl
 #define sz(s)	(int)(s.size())
 #define OO 0x3f3f3f3f3f3f3f3fLL
-const int oo = 0x3f3f3f3f, Mod = 1e9 + 7;
+#define oo 0x3f3f3f3f
 using namespace std;
 typedef long long ll;
+typedef long double ld;
+typedef unsigned long long ull;
 int dx[] { 1, -1, 0, 0, 1, 1, -1, -1 };
 int dy[] { 0, 0, 1, -1, 1, -1, 1, -1 };
 void K_K(){
@@ -31,63 +34,46 @@ void K_K(){
 #endif  !ONLINEJUDGE
 	ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
 }
-const int N = 1e3 + 5;
-string s;
-int dp[N][N];
-string dp1[N][N];
-int sol(int l, int r){
-	if (l >= r)
-		return (l == r ? 1 : 0);
-
-	int &ret = dp[l][r];
-	if (~ret)
-		return ret;
-	ret = 0;
-	int ch1 = 0, ch2 = 0, ch3 = 0;
-	if (s[l] == s[r])
-		ch1 = sol(l + 1, r - 1) + 2;
-	else{
-		ch2 = sol(l + 1, r);
-		ch3 = sol(l, r - 1);
-	}
-	return ret = max({ ch1, ch2, ch3 });
+const int N = 1e3 + 2;
+string s; int dp[N][N];
+int solve(int i, int j){
+	if (i >= j)return (i == j);
+	int &ret = dp[i][j];
+	if (~ret)return ret; ret = 0;
+	if (s[i] == s[j])ret = solve(i + 1, j - 1) + 2;
+	else
+		ret = max(solve(i + 1, j), solve(i, j - 1));
+	return ret;
 }
-string build(int l, int r){
-	if (l >= r)
-		return (l == r ? string(1, s[l]) : string());
-
-	string &ret = dp1[l][r];
-	if (sz(ret))
-		return ret;
-
-	int rt = sol(l, r);
-	int ch1 = 0, ch2 = 0, ch3 = 0;
-	if (s[l] == s[r]){
-		ch1 = sol(l + 1, r - 1) + 2;
+string dp2[N][N];
+string build(int i, int j){
+	if (i >= j){
+		if (i == j)return string(1, s[i]);
+		return "";
 	}
+	string &ret = dp2[i][j];
+	if (ret != "")return ret;
+	if (s[i] == s[j])
+		return ret = s[i] + build(i + 1, j - 1) + s[j];
 	else{
-		ch2 = sol(l + 1, r);
-		ch3 = sol(l, r - 1);
+		int ans = solve(i, j);
+		int ch1 = solve(i + 1, j), ch2 = solve(i, j - 1);
+		if (ans == ch1 && ans == ch2)
+			return ret = min(build(i + 1, j), build(i, j - 1));
+		else if (ans == ch1)
+			return ret = build(i + 1, j);
+		else
+			return ret = build(i, j - 1);
 	}
-	if (rt == ch1)
-		return ret = s[l] + build(l + 1, r - 1) + s[r];
-
-	if (ch2 == ch3)
-		return ret = min(build(l + 1, r), build(l, r - 1));
-
-	if (ch2 > ch3)
-		return ret = build(l + 1, r);
-
-	return ret = build(l, r - 1);
 }
 int main(){
 	K_K();
 	while (cin >> s){
-		clr(dp, -1);
-		cout << build(0, sz(s) - 1) << endl;
+		clr(dp, -1); solve(0, sz(s) - 1); 
 		for (int i = 0; i < N; i++){
 			for (int j = 0; j < N; j++)
-				dp1[i][j].clear();
+				dp2[i][j] = "";
 		}
+		cout << build(0, sz(s) - 1) << endl;
 	}
 }
