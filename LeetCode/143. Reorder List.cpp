30cccc97ba7d023bf3dcf8b59e1de59bc3abe16a
Kareem Khaled <53629881+Kareem-Khaled@users.noreply.github.com>
2025-08-14 00:25:28 +0300

    Create 143. Reorder List.cpp

---
+++ LeetCode/143. Reorder List.cpp
@@ -0,0 +1,56 @@
+/*
+██╗  ██╗ █████╗ ██████╗ ███████╗███████╗███╗   ███╗        ██╗  ██╗██╗  ██╗ █████╗ ██╗     ███████╗██████╗
+██║ ██╔╝██╔══██╗██╔══██╗██╔════╝██╔════╝████╗ ████║        ██║ ██╔╝██║  ██║██╔══██╗██║     ██╔════╝██╔══██╗
+█████╔╝ ███████║██████╔╝█████╗  █████╗  ██╔████╔██║        █████╔╝ ███████║███████║██║     █████╗  ██║  ██║
+██╔═██╗ ██╔══██║██╔══██╗██╔══╝  ██╔══╝  ██║╚██╔╝██║        ██╔═██╗ ██╔══██║██╔══██║██║     ██╔══╝  ██║  ██║
+██║  ██╗██║  ██║██║  ██║███████╗███████╗██║ ╚═╝ ██║███████╗██║  ██╗██║  ██║██║  ██║███████╗███████╗██████╔╝
+╚═╝  ╚═╝╚═╝  ╚═╝╚═╝  ╚═╝╚══════╝╚══════╝╚═╝     ╚═╝╚══════╝╚═╝  ╚═╝╚═╝  ╚═╝╚═╝  ╚═╝╚══════╝╚══════╝╚═════╝
+Believe in yourself ,( try and try and then try ), You can do it
+*/
+// Problem Link : https://leetcode.com/problems/reorder-list/description
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    void reorderList(ListNode* head) {
+        ListNode* a = head;
+        ListNode* half = head;
+
+        // get the half
+        while(a && a->next){
+            a = a->next->next;
+            half = half->next;
+        }
+
+        // reverse the second half
+        ListNode* pre = NULL;
+        while(half){
+            ListNode* nxt = half->next;
+            half->next = pre;
+            pre = half;
+            half = nxt;
+        }
+        half = pre;
+
+        // merge the two halfs
+        while(head && half){
+            ListNode* nxt = head->next;
+            ListNode* nxt2 = half->next;
+            head->next = half;
+            half->next = nxt;
+            head = nxt;
+            half = nxt2;
+        }
+
+        if (head) head->next = NULL;
+    }
+};
